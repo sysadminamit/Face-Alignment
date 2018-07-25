791b4a4b6e7574628ac29c8310888d10723d46c1
sysadminamit <amitc8250@gmail.com>
2018-07-25 16:15:32 +0530

    Second

--- face_align.py
+++ face_align.py
@@ -3,6 +3,10 @@ from helpers import FACIAL_LANDMARKS_IDXS
 from helpers import shape_to_np
 import numpy as np
 import cv2
+import os
+
+path = "C:/Users/AMIT/Desktop/prediction/new"
+counter =0
 
 class FaceAligner:
     def __init__(self, predictor, desiredLeftEye=(0.35, 0.35),
@@ -71,47 +75,3 @@ class FaceAligner:
         return output
 
 
-# import the necessary packages
-from imutils.face_utils import FaceAligner
-from imutils.face_utils import rect_to_bb
-import argparse
-import imutils
-import dlib
-import cv2
-
-# construct the argument parser and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-p", "--shape-predictor", required=False,
-                help="path to facial landmark predictor")
-ap.add_argument("-i", "--image", required=False,
-                help="path to input image")
-args = vars(ap.parse_args())
-args["shape_predictor"] = 'shape_predictor_68_face_landmarks.dat'
-args['image'] = 'image/anir.jpeg'
-
-# initialize dlib's face detector (HOG-based) and then create
-# the facial landmark predictor and the face aligner
-detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor(args["shape_predictor"])
-fa = FaceAligner(predictor, desiredFaceWidth=256)
-
-# load the input image, resize it, and convert it to grayscale
-image = cv2.imread(args["image"])
-image = imutils.resize(image, width=800)
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-# show the original input image and detect faces in the grayscale
-# image
-cv2.imshow("Input", image)
-rects = detector(gray, 2)
-for rect in rects:
-    # extract the ROI of the *original* face, then align the face
-    # using facial landmarks
-    (x, y, w, h) = rect_to_bb(rect)
-    faceOrig = imutils.resize(image[y:y + h, x:x + w], width=256)
-    faceAligned = fa.align(image, gray, rect)
-
-    # display the output images
-    cv2.imshow("Original", faceOrig)
-    cv2.imshow("Aligned", faceAligned)
-    cv2.waitKey(0)
---
+++ face_align_controller.py
@@ -0,0 +1,48 @@
+# import the necessary packages
+from imutils.face_utils import FaceAligner
+from imutils.face_utils import rect_to_bb
+import argparse
+import imutils
+import dlib
+import cv2
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--shape-predictor", required=False,
+                help="path to facial landmark predictor")
+ap.add_argument("-i", "--image", required=False,
+                help="path to input image")
+args = vars(ap.parse_args())
+args["shape_predictor"] = 'shape_predictor_68_face_landmarks.dat'
+args['image'] = 'image/img.jpg'
+
+# initialize dlib's face detector (HOG-based) and then create
+# the facial landmark predictor and the face aligner
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(args["shape_predictor"])
+fa = FaceAligner(predictor, desiredFaceWidth=480)
+
+# load the input image, resize it, and convert it to grayscale
+image = cv2.imread(args["image"])
+image = imutils.resize(image, width=480)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# show the original input image and detect faces in the grayscale
+# image
+#cv2.imshow("Input", image)
+rects = detector(gray, 2)
+for rect in rects:
+    # extract the ROI of the *original* face, then align the face
+    # using facial landmarks
+    (x, y, w, h) = rect_to_bb(rect)
+    faceOrig = imutils.resize(image[y:y + h, x:x + w], width=256)
+    faceAligned = fa.align(image, gray, rect)
+
+    # display the output images
+    # file_name = str(counter) + '.jpg'
+    # cv2.imwrite(os.path.join(path, file_name),faceAligned)
+    # cv2.imshow("Original", faceOrig)
+    cv2.imshow("Aligned", faceAligned)
+    #print(faceAligned.shape)
+
+    cv2.waitKey(0)
